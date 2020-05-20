e3bbbcda55db2324742263dc205037a4a6fc44c0
Ckwei <kkww0259@gmail.com>
2020-05-20 18:22:49 +0800

    add

--- 10_2_MQTT_Python/main.cpp
+++ 10_2_MQTT_Python/main.cpp
@@ -2,6 +2,28 @@
 #include "MQTTNetwork.h"
 #include "MQTTmbed.h"
 #include "MQTTClient.h"
+#include "fsl_port.h"
+#include "fsl_gpio.h"
+#define UINT14_MAX        16383
+// FXOS8700CQ I2C address
+#define FXOS8700CQ_SLAVE_ADDR0 (0x1E<<1) // with pins SA0=0, SA1=0
+#define FXOS8700CQ_SLAVE_ADDR1 (0x1D<<1) // with pins SA0=1, SA1=0
+#define FXOS8700CQ_SLAVE_ADDR2 (0x1C<<1) // with pins SA0=0, SA1=1
+#define FXOS8700CQ_SLAVE_ADDR3 (0x1F<<1) // with pins SA0=1, SA1=1
+// FXOS8700CQ internal register addresses
+#define FXOS8700Q_STATUS 0x00
+#define FXOS8700Q_OUT_X_MSB 0x01
+#define FXOS8700Q_OUT_Y_MSB 0x03
+#define FXOS8700Q_OUT_Z_MSB 0x05
+#define FXOS8700Q_M_OUT_X_MSB 0x33
+#define FXOS8700Q_M_OUT_Y_MSB 0x35
+#define FXOS8700Q_M_OUT_Z_MSB 0x37
+#define FXOS8700Q_WHOAMI 0x0D
+#define FXOS8700Q_XYZ_DATA_CFG 0x0E
+#define FXOS8700Q_CTRL_REG1 0x2A
+#define FXOS8700Q_M_CTRL_REG1 0x5B
+#define FXOS8700Q_M_CTRL_REG2 0x5C
+#define FXOS8700Q_WHOAMI_VAL 0xC7
 
 // GLOBAL VARIABLES
 WiFiInterface *wifi;
@@ -11,6 +33,45 @@ volatile int message_num = 0;
 volatile int arrivedcount = 0;
 volatile bool closed = false;
 const char* topic = "Mbed";
+float X=0.0, Y=0.0, Z=0.0;
+EventQueue queue(32 * EVENTS_EVENT_SIZE);
+Thread t;
+
+I2C i2c( PTD9,PTD8);
+//Serial pc(USBTX, USBRX);
+int m_addr = FXOS8700CQ_SLAVE_ADDR1;
+
+void FXOS8700CQ_readRegs(int addr, uint8_t * data, int len) {
+   char t = addr;
+   i2c.write(m_addr, &t, 1, true);
+   i2c.read(m_addr, (char *)data, len);
+}
+
+void FXOS8700CQ_writeRegs(uint8_t * data, int len) {
+   i2c.write(m_addr, (char *)data, len);
+}
+
+void record(void) {
+   float x=0.0, y=0.0, z=0.0;
+   int16_t acc16;
+   uint8_t res[6];
+   FXOS8700CQ_readRegs(FXOS8700Q_OUT_X_MSB, res, 6);
+   acc16 = (res[0] << 6) | (res[1] >> 2);
+   if (acc16 > UINT14_MAX/2)
+     acc16 -= UINT14_MAX;
+   x = ((float)acc16) / 4096.0f;
+   acc16 = (res[2] << 6) | (res[3] >> 2);
+   if (acc16 > UINT14_MAX/2)
+     acc16 -= UINT14_MAX;
+   y = ((float)acc16) / 4096.0f;
+   acc16 = (res[4] << 6) | (res[5] >> 2);
+   if (acc16 > UINT14_MAX/2)
+     acc16 -= UINT14_MAX;
+   z = ((float)acc16) / 4096.0f;
+   X = x;
+   Y = y;
+   Z = z;
+}
 
 Thread mqtt_thread(osPriorityHigh);
 EventQueue mqtt_queue;
@@ -27,10 +88,29 @@ void messageArrived(MQTT::MessageData& md) {
 }
 
 void publish_message(MQTT::Client<MQTTNetwork, Countdown>* client) {
+      /*float x=0.0, y=0.0, z=0.0;
+      int16_t acc16;
+      uint8_t res[6];
+      FXOS8700CQ_readRegs(FXOS8700Q_OUT_X_MSB, res, 6);
+      acc16 = (res[0] << 6) | (res[1] >> 2);
+      if (acc16 > UINT14_MAX/2)
+      acc16 -= UINT14_MAX;
+      x = ((float)acc16) / 4096.0f;
+      acc16 = (res[2] << 6) | (res[3] >> 2);
+      if (acc16 > UINT14_MAX/2)
+      acc16 -= UINT14_MAX;
+      y = ((float)acc16) / 4096.0f;
+      acc16 = (res[4] << 6) | (res[5] >> 2);
+      if (acc16 > UINT14_MAX/2)
+      acc16 -= UINT14_MAX;
+      z = ((float)acc16) / 4096.0f;*/
+      queue.call_every(0.5,record);
+      
+
       message_num++;
       MQTT::Message message;
       char buff[100];
-      sprintf(buff, "QoS0 Hello, Python! #%d", message_num);
+      sprintf(buff, "%f, %f, %f\n", X * 1000.0, Y * 1000.0, Z * 1000.0);
       message.qos = MQTT::QOS0;
       message.retained = false;
       message.dup = false;
@@ -44,6 +124,7 @@ void publish_message(MQTT::Client<MQTTNetwork, Countdown>* client) {
 void close_mqtt() {
       closed = true;
 }
+
 int main() {
 
       wifi = WiFiInterface::get_default_instance();
@@ -81,9 +162,19 @@ int main() {
       if (client.subscribe(topic, MQTT::QOS0, messageArrived) != 0){
             printf("Fail to subscribe\r\n");
       }
-
+      
+      //initFXOS8700Q();
+      t.start(callback(&queue, &EventQueue::dispatch_forever));
+      
       mqtt_thread.start(callback(&mqtt_queue, &EventQueue::dispatch_forever));
+      while(1){
+            record();
+            publish_message(&client);
+            wait(0.5);
+      }
+      
       btn2.rise(mqtt_queue.event(&publish_message, &client));
+      //mqtt_queue.call_every(0.5, publish_message);
       btn3.rise(&close_mqtt);
       int num = 0;
       while (num != 5) {
